37c981a223a2afcf6b4fba52de6f1ac1c18d80d3
DESKTOP-T9CQTOI\avaen <avaenthomas@gmail.com>
2020-04-16 15:26:27 -0500

    Add hover FX to the button and fixed text size bug

---
+++ Simple Dialogue/Assets/Scripts/HoverColor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HoverColor : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Text text;
+    private Color initialColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialColor = text.color;
+    }
+
+    // When the mouse enters the button area
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.color = Color.white;
+    }
+
+    // When the mouse exits the button area
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.color = initialColor;
+    }
+}
